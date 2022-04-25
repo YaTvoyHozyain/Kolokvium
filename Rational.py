622a7350ce3864c3bc40c7fb547f644967111773
YaTvoyHozyain <103387646+YaTvoyHozyain@users.noreply.github.com>
2022-04-25 12:55:26 +0300

    Update Rational.py

--- Rational.py
+++ Rational.py
@@ -1,172 +1,197 @@
-
 # Q-1 Черникова
-# cокращение дроби
-# дано: рац. дробь - числ-ль num и знам-ль denom
+# Сокращение дроби
 # использовать:
-# Z-1 ABS_Z_N абсолютная величина числа, результат - нат.число
-# N-13 GCF_NN_N НОД нат.чисел
-# Z-9 DIV_ZZ_Z частное от деления целого на целое (делитель не ноль)
-def RED_Q_Q(num, denom):
-    if num != 0:
-        # делаем числ-ль натуральным
-        num_nat = ABS_Z_N(num)
-        # находим НОД натуральных числ-ля и знам-ля
+# {Z-1, ABS_Z_N} Aбсолютная величина числа, результат - нат.число
+# {N-13, GCF_NN_N} НОД нат.чисел
+# {Z-9, DIV_ZZ_} Частное от деления целого на целое (делитель не ноль)
+# вход: num, numb, denom - массив цифр числителя, знак дроби, массив цифр знаменателя
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def RED_Q_Q(num, numb, denom):
+    # проверяем чилитель на 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(num, numb) != 0:
+        # делаем числитель натуральным {Z-1, ABS_Z_N}
+        num_nat = ABS_Z_N(num, numb)
+        # находим НОД натуральных числителя и знаменателя {N-13, GCF_NN_N}
         gcf = GCF_NN_N(num_nat, denom)
-        # Z-4 TRANS_N_Z преобразование натурального в целое
+        # делим натуральный знаменатель на натуральный НОД {N-11, DIV_NN_N}
+        denom = DIV_NN_N(denom, gcf)
+        # преобразуем натуральный НОД в целое число {Z-4, TRANS_N_Z}
         gcf = TRANS_N_Z(gcf)
-        # делим цел.числ-ль на цел. НОД
-        num = DIV_ZZ_Z(num[0], num[2], gcf[0], gcf[2])
-        # делим нат.знам-ль на НОД
-        # (N-11 DIV_NN_N частное от деления с остатком нат. числа на меньшее/равное, ненулевое)
-        denom = DIV_NN_N(denom[0], denom[1], gcf[0], gcf[0])
-        # собираем рац.дробь
-        qres = (num, denom)
+        # делим целый числитель на целый НОД {Z-9, DIV_ZZ_Z}
+        num = DIV_ZZ_Z(num, numb, gcf[0], gcf[1])
+        return num, numb, denom
     else:
-        # если числ-ль 0, дробь 0
-        qres = 0, 1, 0
-    return qres
+        # если числитель 0, вернуть нулевую дробь [0], 0, [1] (1,тк знаменатель не мб 0) {Q-3,  TRANS_Z_Q}
+        qzero = TRANS_Z_Q(num, numb)
+        return qzero[0], qzero[1], qzero[2]
 
 
 # Q-2 Селиверстов
-# Проверка на целое, если рациональное число является целым, то <да>, иначе <нет>
-def INT_Q_B(A, B):
-    if len(B) == 1 and B[0] == 1:
-        return True
-    elif len(A) == 1 and A[0] == 0:
-        return True
-    elif A == B:
-        return True
+# Проверка на целое, если рациональное число является целым, то 1, иначе 0
+# вход: num, numb, denom - массив цифр числителя, знак дроби, массив цифр знаменателя
+# выход: 0 или 1
+def INT_Q_B(num, numb, denom):
+    # проверяем целый числитель на 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(num, numb) != 0:
+        # сокращаем дробь {Q-1, RED_Q_Q}
+        q = RED_Q_Q(num, numb, denom)
+        # если знаменатель равен 1 - дробь целая {N-1, COM_NN_D}
+        if COM_NN_D(q[2], [1]) == 1:
+            return 1
+        else:
+            return 0
+    # если числитель 0 - дробь 0, т.е. целое
     else:
-        return False
+        return 1
     
 
 # Q-3 Моисеев
 # Преобразование целого в дробное
-def TRANS_Z_Q(a):
-    # возвращает массив [a,b], где
-    # a - числитель, b - знаменатель
-    t = [a, 1]
-    return t
-
+# вход: A, b - массив цифр целого числа, его знак
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def TRANS_Z_Q(A, b):
+    # если целое число не 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(A, b) != 0:
+        # в числитель - массив цифр, в знак дроби - знак целого, в знаменатель - 1
+        num, numb, denom = A, b, [1]
+        return num, numb, denom
+    # если числитель 0 - дробь 0:
+    else:
+        # иначе вернуть 0 в виде дроби (1 в знаменателе, тк он не мб 0)
+        return [0], 0, [1]
 
 # Q-4 Махаев
-# Преобразование дробного в целое (если знаменатель равен 1),
-def TRANS_Q_Z(A,B):
-    # По хорошему, здесь в начале должна быть функция Q-1,
-    # чтобы сделать дробь несократимой, и потом уже преобразовывать в целое
-    zero_element = 0
-    count_element = 0
-    for i in range(len(B)):
-        if B[i] == 0:
-            zero_element = zero_element + 1
-        count_element = count_element + 1
-        # Считаем количество элементов и нулевых элементов в знаменателе
-    if (zero_element == count_element - 1) and (B[zero_element] == 1):
-        # Если количество элементов больше чем количество нулевых элементов на единицу
-        # и последний элемент знаменателя равен единице,то возвращаем только числитель
-        return A
-    
+# Преобразование дробного в целое (если знаменатель равен 1)
+# вход: num, numb, denom - массив цифр числителя, знак дроби, массив цифр знаменателя
+# выход: массив цифр целого числа, его знак
+def TRANS_Q_Z(num, numb, denom):
+    # проверим дробь на целое, результат сохраним {Q-2, INT_Q_B}
+    # INT_Q_B вернет 1, если дробь 0 или её знаменатель 1, иначе 2
+    result1 = INT_Q_B(num, numb, denom)
+    # если результат равен 1 - дробь целая {N-1, COM_NN_D}
+    if COM_NN_D([result1], [1]) == 1:
+        # если дробь(числитель) не 0, вернуть числитель и знак {Z-2, POZ_Z_D}
+        if POZ_Z_D(num, numb) != 0:
+            return num, numb
+        else:
+            # иначе сделать числитель целым числом 0 со знаком 0 {Z-4, TRANS_N_Z}
+            qzero = TRANS_N_Z(num)
+            return qzero[0], qzero[1]
+    else:
+        print("This fraction is NOT integer.")
+
     
 # Q-5 Булацкий 
 # Сложение дробей
-def ADD_QQ_Q(A, B, C, D):
-    tempB = B.copy()
-    tempD = D.copy()
-    sum3 = MUL_NN_N(tempB, tempD)
-    sum1 = MUL_ZZ_Z(A, TRANS_N_Z(D))
-    sum2 = MUL_ZZ_Z(TRANS_N_Z(B), C)
-    temp = ADD_ZZ_Z(sum1, sum2)
-    return RED_Q_Q([temp, sum3])
+# использовать:
+# {N-14, LCM_NN_N} НОК натуральных чисел
+# {Z-8, MUL_ZZ_Z} Умножение целых чисел
+# {Z-6, ADD_ZZ_Z} Сложение целых чисел
+# вход: num1, num1b, denom1 - массив цифр числителя 1, знак дроби 1, массив цифр знаменателя 1
+# num2, num2b, denom2 - массив цифр числителя 2, знак дроби 2, массив цифр знаменателя 2
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def ADD_QQ_Q(num1, numb1, denom1, num2, numb2, denom2):
+    # находим НОК знаменателей {N-14, LCM_NN_N}
+    lcm = LCM_NN_N(denom1, denom2)
+    # находим коэффициенты для домножения на числители: делим НОК на каждый знаменатель {N-11, DIV_NN_N}
+    koef1 = DIV_NN_N(lcm, denom1)
+    koef2 = DIV_NN_N(lcm, denom2)
+    # делаем коэффициенты целыми числами {Z-4, TRANS_N_Z}
+    koef1 = TRANS_N_Z(koef1)
+    koef2 = TRANS_N_Z(koef2)
+    # умножаем их на числители: num1 на k2, num2 на k1 {Z-8, MUL_ZZ_Z}
+    num1 = MUL_ZZ_Z(num1, numb1, koef2[0], koef2[1])
+    num2 = MUL_ZZ_Z(num2, numb2, koef1[0], koef1[1])
+    # складываем числители {Z-6, ADD_ZZ_Z}
+    numres = ADD_ZZ_Z(num1[0], num1[1], num2[0], num2[1])
+    # если полученный числитель не 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(numres[0], numres[1]) != 0:
+        # сократим полученную дробь {Q-1, RED_Q_Q}
+        qres = RED_Q_Q(numres[0], numres[1], lcf)
+        return qres[0], qres[1], qres[2]
+    else:
+        # иначе вернуть нулевую дробь [0], 0, [1] (1,тк знаменатель не мб 0) {Q-3, TRANS_Z_Q}
+        qzero = TRANS_Z_Q(num, numb)
+        return qzero[0], qzero[1], qzero[2]
 
 
 # Q-6 Прейгель
-# вычитание дробей
-def SUB_QQ_Q(q1, k1, q2, k2):
-    #определение знаков и приведение чисел к виду массива(числители и знаменатели)
-    Q1 = []
-    Q2 = []
-    K1 = []
-    K2 = []
-    if(str(q1)[0] == "-"):
-        bq1 = 1
-        nq1 = len(str(q1))-1
-        for x in range(1, len(str(q1))):
-            Q1.append(int(str(q1)[x]))
+# Вычитание дробей
+# использовать:
+# {N-14, LCM_NN_N} НОК натуральных чисел
+# {Z-8, MUL_ZZ_Z} Умножение целых чисел
+# {Z-7, SUB_ZZ_Z} Вычитание целых чисел
+# вход: num1, num1b, denom1 - массив цифр числителя 1, знак дроби 1, массив цифр знаменателя 1
+# num2, num2b, denom2 - массив цифр числителя 2, знак дроби 2, массив цифр знаменателя 2
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def SUB_QQ_Q(num1, numb1, denom1, num2, numb2, denom2):
+    # находим НОК знаменателей
+    lcm = LCM_NN_N(denom1, denom2)
+    # находим коэффициенты для домножения на числители: делим НОК на каждый знаменатель {N-11, DIV_NN_N}
+    koef1 = DIV_NN_N(lcm, denom1)
+    koef2 = DIV_NN_N(lcm, denom2)
+    # делаем коэффициенты целыми числами {Z-4, TRANS_N_Z}
+    koef1 = TRANS_N_Z(koef1)
+    koef2 = TRANS_N_Z(koef2)
+    # умножаем их на числители: num1 на k2, num2 на k1 {Z-8, MUL_ZZ_Z}
+    num1 = MUL_ZZ_Z(num1, numb1, koef2[0], koef2[1])
+    num2 = MUL_ZZ_Z(num2, numb2, koef1[0], koef1[1])
+    # вычитаем числители {Z-7, SUB_ZZ_Z}
+    numres = SUB_ZZ_Z(num1[0], num1[1], num2[0], num2[1])
+    # если полученный числитель не 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(numres[0], numres[1]) != 0:
+        # сократим полученную дробь {Q-1, RED_Q_Q}
+        qres = RED_Q_Q(numres[0], numres[1], lcf)
+        return qres[0], qres[1], qres[2]
     else:
-        bq1 = 0
-        nq1 = len(str(q1))
-        for x in str(q1):
-            Q1.append(int(x))
-    if(str(q2)[0] == "-"):
-        bq2 = 1
-        nq2 = len(str(q2))-1
-        for x in range(1, len(str(q2))):
-            Q2.append(int(str(q2)[x]))
-    else:
-        bq2 = 0
-        nq2 = len(str(q2))
-        for x in str(q2):
-            Q2.append(int(x))
-    bk1 = 0
-    bk2 = 0
-    nk1 = len(str(k1))
-    nk2 = len(str(k2))
-    for k in str(k1):
-        K1.append(int(k))
-    for k in str(k2):
-        K2.append(int(k))
-    #умножение первого числителя на второй знаменатель
-    bq1,nq1,Q1 = MUL_ZZ_Z(bq1,nq1,Q1,bk2,nk2,K2)
-    #перемножение знаменателей
-    K3 = MUL_NN_N(K1, K2)
-    #умножение второгт числителя на первый знаменатель
-    bq2,nq2,Q2 = MUL_ZZ_Z(bq2,nq2,Q2,bk1,nk1,K1)
-    #вычитание второго числителя из первого
-    Q3 = SUB_ZZ_Z(Q1, bq1, nq1, Q2, bq2, nq2)
-    strk3 = ""
-    strq3 = ""
-    for x in K3:
-        strk3 += str(x)
-    for x in Q3:
-        strq3 += str(x)
-    K3 = int(strk3)
-    Q3 = int(strq3)
-    #сокращение получившейся дроби
-    return RED_Q_Q(Q3, K3) 
+        # иначе вернуть нулевую дробь [0], 0, [1] (1,тк знаменатель не мб 0) {Q-3,  TRANS_Z_Q}
+        qzero = TRANS_Z_Q(num, numb)
+        return qzero[0], qzero[1], qzero[2]
 
 
 # Q-7 Черникова
-# умножение дробей
-# дано: рац. дроби - числ-ли num1, num2 и знам-ли denom1, denom2
+# Умножение дробей
 # использовать:
-def MUL_QQ_Q(num1, denom1, num2, denom2):
-    if num1[2] or num2[2] != [0]:
-        # умножение целых числ-й
-        qnum = MUL_ZZ_Z(num1[0], num1[1], num1[2], num2[0], num2[1], num2[2])
-        # умножение нат. знам-й
-        # N-8 MUL_NN_N умножение натуральных чисел
+# {Z-8, MUL_ZZ_Z} Умножение целых чисел
+# вход: num1, num1b, denom1 - массив цифр числителя 1, знак дроби 1, массив цифр знаменателя 1
+# num2, num2b, denom2 - массив цифр числителя 2, знак дроби 2, массив цифр знаменателя 2
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def MUL_QQ_Q(num1, num1b, denom1, num2, num2b, denom2):
+    # если числители не равны 0 {Z-2, POZ_Z_D}
+    if POZ_Z_D(num1, num1b) != 0 and POZ_Z_D(num2, num2b) != 0:
+        # умножим целые числители {Z-8, MUL_ZZ_Z}
+        qnum = MUL_ZZ_Z(num1, num1b, num2, num2b)
+        # умноженим натуральные знаменатели {N-8, MUL_NN_N}
         qdenom = MUL_NN_N(denom1, denom2)
-        # сокращаем рац. дробь
-        # Q-1 RED_Q_Q cокращение дроби
-        qres = RED_Q_Q(qnum, qdenom)
+        # сократим полученную дробь {Q-1, RED_Q_Q}
+        qres = RED_Q_Q(qnum[0], qnum[1], qdenom)
+        return qres[0], qres[1], qres[2]
     else:
-        # если хб один числ-ль 0, дробь 0
-        qres = 0, 1, 0
-    return qres
+        # иначе вернуть нулевую дробь [0], 0, [1] (1,т.к. знаменатель не мб 0) {Q-3,  TRANS_Z_Q}
+        qzero = TRANS_Z_Q(num, numb)
+        return qzero[0], qzero[1], qzero[2]
         
     
 # Q-8 Счастливая
 # Деление дробей (делитель отличен от нуля)
-def DIV_QQ_Q(b1, Z1, n1, N1, b2, Z2, N2):
-    # Z1, Z2 - числители (целое), N1, N2 - знаменатели (натуральное)
-    # b1, b2 - знаки Z1 и Z2 соответственно
-    # n1, n2 - номера старшей позиции Z1 и Z2 соответственно
-    # Переворачиваем вторую дробь
-    t = N2
-    N2 = Z2
-    Z2 = t
-    # Перемножаем числители (целые числа)
-    chis = MUL_ZZ_Z(b1, n1, Z1, b2, n2, Z2)
-    # Перемножаем знаменатели (натуральные числа)
-    znam = MUL_NN_N(N1, N2)
-    return chis, znam
+# использовать:
+# {Z-8, MUL_ZZ_Z} Умножение целых чисел
+# вход: num1, num1b, denom1 - массив цифр числителя 1, знак дроби 1, массив цифр знаменателя 1
+# num2, num2b, denom2 - массив цифр числителя 2, знак дроби 2, массив цифр знаменателя 2
+# выход: массив цифр числителя, знак дроби, массив цифр знаменателя
+def DIV_QQ_Q(num1, num1b, denom1, num2, num2b, denom2):
+    # если числители не равны 0
+    if POZ_Z_D(num1, num1b) != 0 and POZ_Z_D(num2, num2b) != 0:
+        # переворачиваем вторую дробь
+        num2, denom2 = denom2, num2
+        # умножим целые числители {Z-8, MUL_ZZ_Z}
+        qnum = MUL_ZZ_Z(num1, num1b, num2, num2b)
+        # умноженим натуральные знаменатели {N-8, MUL_NN_N}
+        qdenom = MUL_NN_N(denom1, denom2)
+        # сократим полученную дробь {Q-1, RED_Q_Q}
+        qres = RED_Q_Q(qnum[0], qnum[1], qdenom)
+        return qres[0], qres[1], qres[2]
+    else:
+        # иначе вернуть нулевую дробь [0], 0, [1] (1,т.к. знаменатель не мб 0) {Q-3,  TRANS_Z_Q}
+        qzero = TRANS_Z_Q(num, numb)
+        return qzero[0], qzero[1], qzero[2]
