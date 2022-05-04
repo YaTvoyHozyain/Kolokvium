edb5908401167efc97dfd9446d5efa251e7d0de3
YaTvoyHozyain <103387646+YaTvoyHozyain@users.noreply.github.com>
2022-05-04 13:10:05 +0300

    Update дополнения.py

--- дополнения.py
+++ дополнения.py
@@ -1,13 +1,10 @@
 # Преобразование числа в массив чисел
 def INT_TO_ARR(A):
+    A = str(A)
     B = [0] * len(A)
     for i in range(len(A)):
         B[i] = int(A[i])
     return B
-
-A = input("Enter the number: ")
-
-print(INT_TO_ARR(A))
 
 
 # Преобразование массива чисел в число
